5bec62b43954a8eb973179348bb196192bacae58
X3RMINATION <nicolaeungur@gmail.com>
2022-01-18 20:17:42 -0800

    Started Work on ENergy storage

---
+++ src/main/java/com/github/x3rmination/common/blocks/tile_entities/energy_storage/EnergyStorageScreen.java
@@ -0,0 +1,17 @@
+package com.github.x3rmination.common.blocks.tile_entities.energy_storage;
+
+import com.mojang.blaze3d.matrix.MatrixStack;
+import net.minecraft.client.gui.screen.inventory.ContainerScreen;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.util.text.ITextComponent;
+
+public class EnergyStorageScreen extends ContainerScreen<EnergyStorageContainer> {
+    public EnergyStorageScreen(EnergyStorageContainer menu, PlayerInventory playerInventory, ITextComponent title) {
+        super(menu, playerInventory, title);
+    }
+
+    @Override
+    protected void renderBg(MatrixStack pMatrixStack, float pPartialTicks, int pX, int pY) {
+
+    }
+}
